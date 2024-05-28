abd2abf1522b100c69c36574c296f44a684c04dc
nature1339 <76989460+nature1339@users.noreply.github.com>
2024-05-28 21:05:47 +0900

    3-2

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="bg-gray-300 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-2xl w-full">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col">
+            <span className="text-gray-600 font-semibold-mb-1">In transit</span>
+            <span className="text-4xl font-semibold">Coolblue</span>
+          </div>
+          <div className="my-2 flex items-center gap-2">
+            <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full">
+              Today
+            </span>
+            <span>9:30-10:30</span>
+          </div>
+          <div className="flex justify-between items-center mt-5 text-gray-600">
+            <span>Expected</span>
+            <span>Sorting center</span>
+            <span>In transit</span>
+            <span>Delivered</span>
+          </div>
+        </div>
+        <div className="relative">
+          <div className="bg-gray-200 top-10 absolute rounded-full w-full h-2" />
+          <div className="bg-green-400 absolute rounded-full w-1/2 h-2" />
+        </div>
+      </div>
+    </main>
+  );
 }
