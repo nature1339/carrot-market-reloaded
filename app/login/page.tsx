31a9ba6beae533dba0e6389380900807f3c62d0b
nature1339 <76989460+nature1339@users.noreply.github.com>
2024-11-28 16:21:35 +0900

    use client

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,13 @@
-"use client";
-
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form_input";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
-  const onClick = async () => {
-    const response = await fetch("/www/users", {
-      method: "POST",
-      body: JSON.stringify({
-        username: "sylee",
-        password: "1234",
-      }),
-    });
-    console.log(await response.json());
-  };
+  async function handleForm(formData: FormData) {
+    "use server";
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("logged in!");
+  }
 
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -22,9 +15,10 @@ export default function LogIn() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
+        <FormInput name="email" placeholder="Email" required errors={[]} />
         <FormInput
+          name="password"
           type="password"
           placeholder="password"
           required
